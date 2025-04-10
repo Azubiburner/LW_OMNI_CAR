26201831b459c05aeccba3490d237a2bc044c97a
NickJokers <nick.jokers94@gmail.com>
2025-04-10 08:33:05 +0200

    Update Lichter.py
    unnötige Initialisierungen gelöscht

--- Lichter.py
+++ Lichter.py
@@ -2,7 +2,7 @@ import time
 import curses
 from rpi_ws281x import PixelStrip, Color
 import board
-import RPi.GPIO as GPIO
+
 from adafruit_motorkit import MotorKit
 
 LED_COUNT = 4
@@ -24,40 +24,6 @@ kit.motor3.throttle = 0
 kit.motor4.throttle = 0
 
 S = 1.0  # Geschwindigkeit
-
-# Initialisieren der GPIO-Pins für die Motorimpulse
-PULSE_M1 = 16
-PULSE_M2 = 19
-PULSE_M3 = 20
-PULSE_M4 = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PULSE_M1, GPIO.IN)
-GPIO.setup(PULSE_M2, GPIO.IN)
-GPIO.setup(PULSE_M3, GPIO.IN)
-GPIO.setup(PULSE_M4, GPIO.IN)
-
-# Zähler für die Impulse der Motoren
-m1 = 0
-m2 = 0
-m3 = 0
-m4 = 0
-
-# Funktion zur Handhabung der Impulse
-def handler_M1(channel):
-    global m1
-    m1 += 1
-
-def handler_M2(channel):
-    global m2
-    m2 += 1
-
-def handler_M3(channel):
-    global m3
-    m3 += 1
-
-def handler_M4(channel):
-    global m4
-    m4 += 1
 
 def are_motors_active():
     if kit.motor1.throttle != 0 or kit.motor2.throttle != 0 or kit.motor3.throttle != 0 or kit.motor4.throttle != 0:
